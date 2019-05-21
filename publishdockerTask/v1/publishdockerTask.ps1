e9ab19e1686ab63356fe115965541d7b77c70034
KSACEK <kamil.sacek@navertica.com>
2019-05-21 10:24:14 +0200

    Fix variable name error on upgrade

--- publishdockerTask/v1/publishdockerTask.ps1
+++ publishdockerTask/v1/publishdockerTask.ps1
@@ -14,7 +14,7 @@ try{
     Write-Host "Importing module NVRAppDevOps"
     Import-Module NVRAppDevOps -DisableNameChecking
 
-    $dockerapp = Get-NavContainerAppInfo -containerName $containername  -tenantSpecificProperties | where-object {$_.Name -eq $appname}
+    $dockerapp = Get-NavContainerAppInfo -containerName $containername -tenantSpecificProperties | where-object {$_.Name -eq $appname}
     $app = Get-ChildItem $appfile -Recurse -Filter *.app -Exclude $appfileexclude | Select-Object -Last 1
     $install = -not $dockerapp
     if ($install) {
@@ -25,13 +25,13 @@ try{
     Publish-NavContainerApp -containerName $containername -appFile $app.FullName -SkipVerification:$skipverify -sync -install:$install
 
     if ($dockerapp) {
-        $dockerapp = Get-NavContainerAppInfo -containerName $containername -tenantSpecificProperties | where-object {$_.Name -eq $appname}
+        $dockerapp = Get-NavContainerAppInfo -containerName $containername -tenantSpecificProperties | where-object {$_.Name -eq $appname} | Sort-Object -Property "Version"
         if ($dockerapp.Count -gt 1) {
             foreach($app in $dockerapp) {
                 if ($app.IsInstalled) {
                     $previousVersion = $app
                 }
-                if ($appfile.FullName.Contains($app.Version)) {
+                if ($app.FullName.Contains($app.Version)) {
                     Write-Host "Upgrading from ${$previousVersion.Version} to ${$app.Version}"
                     Start-NavContainerAppDataUpgrade -containerName $containername -appName $appname -appVersion $app.Version
                     $newInstalledApp = $app
