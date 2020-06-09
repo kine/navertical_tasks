995f869fd0bbf7883f7ed0bae0951ad6932bffc3
KSACEK <kamil.sacek@navertica.com>
2020-06-09 20:47:06 +0200

    Make getBCAppVersion and publishdockertask artifact compatible

---
+++ getBCAppVersion/v2/getBCAppVersion.ps1
@@ -0,0 +1,61 @@
+[CmdletBinding()]
+param()
+
+Trace-VstsEnteringInvocation $MyInvocation
+
+try{
+    # Get inputs.
+    $SourceFolder = Get-VstsInput -Name 'SourceFolder' -Require
+    $ContainerName = Get-VstsInput -Name 'ContainerName' -Require
+
+    Write-Host "Importing module NVRAppDevOps"
+    Import-Module NVRAppDevOps -DisableNameChecking
+
+    $FileName = Get-ChildItem -Path $SourceFolder | Select-Object -First 1
+    if (Get-NavContainerPath -containerName $ContainerName -path $FileName) {
+        Write-Host 'Getting app version'
+        $AppInfo = Get-NavContainerAppInfoFile -ContainerName $ContainerName -AppPath $FileName
+    } else {
+        # Create fast container to be able to get the al app order
+        $PWord = ConvertTo-SecureString -String 'Pass@word1' -AsPlainText -Force
+        $User = $env:USERNAME
+        $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$PWord
+        $InternalContainerName = 'BCPS'
+        try {
+            $RepoPath = $SourceFolder
+            $RepoPath = Split-Path -Path $RepoPath -Resolve
+            Write-Host "Creating internal container with $RepoPath shared"
+            New-NavContainer -accept_eula `
+                -accept_outdated `
+                -containerName $InternalContainerName `
+                -artifactUrl 'https://bcartifacts.azureedge.net/businesscentral/sandbox/latest' `
+                -doNotExportObjectsToText `
+                -alwaysPull `
+                -shortcuts "None" `
+                -auth 'Windows' `
+                -Credential $cred `
+                -memoryLimit '4GB' `
+                -updateHosts `
+                -useBestContainerOS `
+                -additionalParameters @("--volume ""$($RepoPath):c:\app""") `
+                -myScripts  @(@{"navstart.ps1" = "Write-Host 'Ready for connections!'";"checkhealth.ps1" = "exit 0"})
+    
+            Write-Host 'Getting app version'
+            $AppInfo = Get-NavContainerAppInfoFile -ContainerName BCPS -AppPath $FileName
+                    
+        } finally  {
+            Write-Host 'Remove internal container'
+            Remove-NavContainer -containerName $InternalContainerName
+        }
+        #Write-Output "##vso[task.setvariable variable=Version;isSecret=false;isOutput=true;]$version"
+    }
+
+    Write-Host "Setting variable Version to value $($AppInfo.Version)"
+    Set-VstsTaskVariable -Name Version -Value $($AppInfo.Version)
+    $VersionShort = "$($AppInfo.Version.Major).$($AppInfo.Version.Minor).$($AppInfo.Version.Build)"
+    Write-Host "Setting variable VersionShort to value $VersionShort"
+    Set-VstsTaskVariable -Name VersionShort -Value $VersionShort
+
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
