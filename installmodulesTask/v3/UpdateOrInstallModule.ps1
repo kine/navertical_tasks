fd4f877cf515190b4ff7b76de889a5ab2fc5acd7
KSACEK <kamil.sacek@navertica.com>
2020-08-03 08:39:53 +0200

    Add allow prerelease switch

--- installmodulesTask/v3/UpdateOrInstallModule.ps1
+++ installmodulesTask/v3/UpdateOrInstallModule.ps1
@@ -10,28 +10,29 @@ try{
 
         param(
             $modulename,
-            $requiredversion
+            $requiredversion,
+            $allowPreRelease
         )
         $ExistingModule = Get-Module $modulename -ListAvailable
         if (-not ($ExistingModule)) {
             Write-Host "Installing module $modulename $requiredversion"
             if ($requiredversion) {
-                install-module -Name $modulename -Scope CurrentUser -Force -RequiredVersion $requiredversion
+                install-module -Name $modulename -Scope CurrentUser -Force -RequiredVersion $requiredversion -AllowPrerelease:$allowPreRelease
             } else {
-                install-module -Name $modulename -Scope CurrentUser -Force
+                install-module -Name $modulename -Scope CurrentUser -Force -AllowPrerelease:$allowPreRelease
             }
         } else {
             if ($requiredversion) {
-                $OnlineModule = Find-Module $modulename -RequiredVersion $requiredversion
+                $OnlineModule = Find-Module $modulename -RequiredVersion $requiredversion -AllowPrerelease:$allowPreRelease
             } else {
-                $OnlineModule = Find-Module $modulename
+                $OnlineModule = Find-Module $modulename -AllowPrerelease:$allowPreRelease
             }
             if ($OnlineModule.Version -ne $ExistingModule[0].Version) {
                 Write-Host "Newer version online exists - updating module $modulename"
                 if ($requiredversion) {
-                    update-module -Name $modulename -Force -RequiredVersion $requiredversion
+                    update-module -Name $modulename -Force -RequiredVersion $requiredversion -AllowPrerelease:$allowPreRelease
                 } else {
-                    update-module -Name $modulename -Force
+                    update-module -Name $modulename -Force -AllowPrerelease:$allowPreRelease
                 }
             } else {
                 Write-Host "Required version of $modulename already installed"
@@ -41,13 +42,14 @@ try{
 
     # Get inputs.
     $UninstallOldVersions = Get-VstsInput -Name 'UninstallOldVersions' -AsBool
+    $AllowPreRelease = Get-VstsInput -Name 'allowPreRelease' -AsBool
     $NVRAppDevOpsVersion = Get-VstsInput -Name 'NVRAppDevOpsVersion' -Default ''
     $bccontainerhelperVersion = Get-VstsInput -Name 'bccontainerhelperVersion' -Default ''
     
     Write-Host "Checking and Installing needed modules"
     Install-PackageProvider nuget -force | Out-Null
-    UpdateOrInstallModule -modulename 'NVRAppDevOps' -requiredversion $NVRAppDevOpsVersion
-    UpdateOrInstallModule -modulename 'bccontainerhelper' -requiredversion $bccontainerhelperVersion
+    UpdateOrInstallModule -modulename 'NVRAppDevOps' -requiredversion $NVRAppDevOpsVersion -allowPreRelease $AllowPreRelease
+    UpdateOrInstallModule -modulename 'bccontainerhelper' -requiredversion $bccontainerhelperVersion -allowPreRelease $AllowPreRelease
     
     if ($UninstallOldVersions) {
         Write-Host "Uninstalling previous versions of NVRAppDevOps"
