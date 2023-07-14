3acf0c91d4f45b74a5e2f7e22ff7f53f2ff62095
ksacek <kamil.sacek@navertica.com>
2023-07-14 10:23:58 +0200

    Add support for additional alc.exe parameters (from version 12.0.12.41479)

--- compileappTask/v4/compileapptask.ps1
+++ compileappTask/v4/compileapptask.ps1
@@ -22,12 +22,18 @@ try {
     $asmProbingPaths = Get-VstsInput -Name 'AsmProbingPaths' -Default ''
     $usePaket = Get-VstsInput -Name 'UsePaket' -AsBool
 
+    $sourceRepositoryUrl = $env:BUILD_REPOSITORY_URI
+    $sourceCommit = $env:BUILD_SOURCEVERSION
+    $buildBy = 'naverticaltasks'
+    $buildUrl = "https://dev.azure.com/$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$($env:SYSTEM_TEAMPROJECT)/_build/results?buildId=$($env:BUILD_BUILDID)"
+
     if ((-not $containername) -and (-not $artifactUrl)) {
         Write-Error "ContainerName or ArtifactUrl must be filled in!"
     }
 
     Write-Host "Importing module NVRAppDevOps"
     Import-Module NVRAppDevOps -DisableNameChecking
+    $NVRAppDevOpsVersion = (get-module NVRAppDevOps).Version
 
     if ($containername) {
         $orderedapps = Get-ALAppOrder -ContainerName $containername -Path $repopath
@@ -48,6 +54,19 @@ try {
             -AsmProbingPaths $asmProbingPaths
     }
     else {
+        if ($NVRAppDevOpsVersion -ge [version]'2.3.0') {
+            Write-Host "Detected NVRAppDevOps newer or equal to v2.3.0, using additional params for alc.exe"
+            $AdditionalParams = @{
+                SourceRepositoryUrl = $sourceRepositoryUrl
+                SourceCommit        = $sourceCommit
+                BuildBy             = $buildBy
+                BuildUrl            = $buildUrl
+            }
+            Write-Host "$($AdditionalParams | convertto-json)"
+        }
+        else {
+            $AdditionalParams = @{}
+        }
         $orderedapps = Get-ALAppOrder -ArtifactUrl $artifactUrl -Path $repopath
         Write-Host "Compiling the apps without container"
         Compile-ALProjectTree -OrderedApps $orderedapps `
@@ -61,7 +80,8 @@ try {
             -EnableUICop $enableUIcop `
             -RulesetFile $rulesetFile `
             -AsmProbingPaths $asmProbingPaths `
-            -UsePaket $usePaket
+            -UsePaket $usePaket `
+            @AdditionalParams
 
     }
 
